23d0f501344d3e5bf815aca314e0ff15f08feebb
Jezron Cardona <jdcardona@student.apc.edu.ph>
2026-01-13 02:33:36 +0000

    Changed contents of App.vue and main.js

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,11 +1,15 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>Hello World!</h1>
 </template>
 
-<style scoped></style>
+<script>
+export default {
+  data() {
+    return {
+      message: 'This is some text'
+    };
+  }
+};
+</script>
+
+<style></style>
