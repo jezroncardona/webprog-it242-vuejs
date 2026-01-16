522b40c5dd84b7aa4a461c30e15feadbe6dc0c39
Jezron Cardona <jdcardona@student.apc.edu.ph>
2026-01-16 10:53:54 +0800

    Test push.

---
+++ firstsfc/src/components/PersonalProfile.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <header><h1>Personal Profile Web page </h1></header>
+        <main>
+        <h4>About Me:</h4>
+
+        <h5>Name:</h5>
+        <p>Jezron D. Cardona</p>
+        <h5>Education / Achievements:</h5>
+        <p>
+        Senior High School: Pasay City Science National High School<br>
+        College: Asia Pacific College<br>
+        </p>
+        <h5>Course:</h5>
+        <p>Bachelor of Science in Information Technology</p>
+        <h5>Experience:</h5>
+        <p>
+        - Developed a discord.by and Discord JDA based bots.<br>
+        - Created a website in Outsystems<br>
+        </p>
+        <h5>Hobbies & Interests:</h5>
+        <p>
+        - Reading<br>
+        - Drawing<br>
+        - Military and Geopolitical Topics<br>
+        - Formula 1<br>
+        </p>
+        <h5>Goals In Life / Dream:</h5>
+        <p>
+        - Own a two-floor property.<br>
+        - Have a stable income.<br>
+        </p>
+        <h4>Picture Gallery:</h4>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/f/f0/Ferrari_Formula_1_lineup_at_the_N%C3%BCrburgring.jpg" alt="Ferrari F1 cars over the years." width="500">
+        <img src="https://media.formula1.com/image/upload/content/dam/fom-website/2018-redesign-assets/Racehub%20header%20images%2016x9/Brazil.jpg" alt="Race restart after Safety Car in Brazil" width="500">
+        </main>
+    </div>
+</template>
+
+<script></script>
+
+<style src="@/components/css/personalprofile.css"></style>
